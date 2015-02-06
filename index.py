f77e0c4eaeade7fc83ed963c26dbdabdf04fb2aa
Mark Smith <smith.h.mark@gmail.com>
2015-02-05 16:25:31 -0800

    beginning of a script to build an index of the GL site.

---
+++ index.py
@@ -0,0 +1,11 @@
+# index.py
+# build an index of documents to be downloaded from the Gulflink site
+
+def index_all():
+    pass
+
+def index_declass():
+    top_browse_url = "http://www.gulflink.osd.mil/search/browse.html"
+
+    dept_page_link_xpath = "/html/body/table/tbody/tr/td/blockquote/ul/li"
+    
