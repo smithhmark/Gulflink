6078616cc797d5108a7ab16f8d56becceddacbf7
Mark Smith <smith.h.mark@gmail.com>
2015-02-07 23:48:05 -0800

    initial steps to building a document index

--- index.py
+++ index.py
@@ -1,11 +1,35 @@
 # index.py
 # build an index of documents to be downloaded from the Gulflink site
+import scrapelib
+from lxml import etree
 
 def index_all():
     pass
 
-def index_declass():
+def _find_decalss_sources(root):
+    dept_page_link_xpath = "/html/body/table/tr/td/blockquote/ul[2]/li/a"
+    pages = root.xpath(dept_page_link_xpath)
+    return pages
+
+def _parse_etree(file_obj, html_parser=None):A
+    if html_parser is None:
+        html_parser = etree.HTMLParser()
+    root = etree.parse(file_obj, html_parser)
+    return root
+
+def index_declass(scraper=None):
+    if scraper is None:
+        scraper = scrapelib.Scraper()
     top_browse_url = "http://www.gulflink.osd.mil/search/browse.html"
 
-    dept_page_link_xpath = "/html/body/table/tbody/tr/td/blockquote/ul/li"
-    
+    tmpfile, resp = scraper.urlretrieve(top_browse_url)
+    print(resp.headers)
+    print(len(resp.content))
+
+    root = _parse_etree(tmpfile)
+
+    print(root)
+
+    return pages, root
+
+
