7a0affb88fae0c09b757b8ed1f43237b2f95d53b
Mark Smith <smith.h.mark@gmail.com>
2015-02-08 00:36:27 -0800

    beginhing to test

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 # build an index of documents to be downloaded from the Gulflink site
 import scrapelib
 from lxml import etree
+import urllib.parse as urls
 
 def index_all():
     pass
@@ -11,25 +12,61 @@ def _find_decalss_sources(root):
     pages = root.xpath(dept_page_link_xpath)
     return pages
 
-def _parse_etree(file_obj, html_parser=None):A
+def _parse_etree(file_obj, html_parser=None):
     if html_parser is None:
         html_parser = etree.HTMLParser()
     root = etree.parse(file_obj, html_parser)
     return root
 
+def _get_agency_paths(scraper):
+    top_browse_url = "http://www.gulflink.osd.mil/search/browse.html"
+    tmpfile, resp = scraper.urlretrieve(top_browse_url)
+    #print(resp.headers)
+    #print(len(resp.content))
+    root = _parse_etree(tmpfile)
+    #print(root)
+    pages = _find_decalss_sources(root)
+    agency_data = []
+    for page in pages:
+        agency_long = page.text
+        agency_path = page.attrib['href']
+        agency_data.append((agency_long, agency_path))
+    return agency_data
+
+def _get_releases(ag_url, scraper):
+    tmpfile, resp = scraper.urlretrieve(ag_url)
+    root = _parse_etree(tmpfile)
+    rel_xpath = "/html/body/table/tr/td[2]/ul/li/a"
+    rel_links = root.xpath(rel_xpath)
+    ret_val = []
+    for release in rel_links:
+        href = release.attrib['href']
+        link = urls.urljoin(ag_url, href)
+        text = release.text
+        text = text.split(':')[1].strip()
+        ret_val.append((text, link))
+    return ret_val
+
+def test_get_releases():
+    scraper = scrapelib.Scraper()
+    test_url = "http://www.gulflink.osd.mil/declassdocs/dia/"
+    return  _get_releases(test_url, scraper)
+
+def _inventory_agency(agency_data, scraper):
+    long_name, path = agency_data
+    ag_url = urls.urljoin("http://www.gulflink.osd.mil/", path)
+    #print(long_name, ag_url)
+    inv = []
+
+    dnp = _get_releases(ag_url, scraper)
+    return inv
+
 def index_declass(scraper=None):
     if scraper is None:
         scraper = scrapelib.Scraper()
-    top_browse_url = "http://www.gulflink.osd.mil/search/browse.html"
 
-    tmpfile, resp = scraper.urlretrieve(top_browse_url)
-    print(resp.headers)
-    print(len(resp.content))
+    agency_data = _get_agency_paths(scraper)
 
-    root = _parse_etree(tmpfile)
-
-    print(root)
-
-    return pages, root
-
-
+    inventory = []
+    for agency in agency_data:
+        inventory.extend(_inventory_agency(agency, scraper))
